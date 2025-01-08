7a6092e0f00a008e3e87a9e730aafed09646e4e3
Troi Williams <40696868+troiwill@users.noreply.github.com>
2025-01-08 10:16:39 -0500

    Updated the README and reorganized code.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # map-to-minigrid-generator
 A tool that converts a screenshot of a Google Map to a Minigrid environment.
+
+## Quick Usage
+
+Use the `convert_image_to_grid` method to convert a Google Maps image into a Minigrid environment.
+```
+from image2minigrid.grid_generator import Image2MinigridGenerator
+
+grid = Image2MinigridGenerator.convert_image_to_grid(
+    image,              # A NumPy array in BGR format (loaded with cv2)
+    width,              # The width of the environment (in Minigrid cells)
+    height,             # The height of the environment (in Minigrid cells)
+    goal_pos,           # The goal position within the Minigrid environment
+    free_space_color,   # Color (BGR format) to determine which cells will be free space
+    occupied_cell_obj,  # The Minigrid WorldObj instance used to represent occupied cells
+    grid_fn,            # The Minigrid Grid class or a subclass used to create the internal Grid.
+)
+```
+
+For more detailed usage, please look at the scripts in the `tests` directory.
+
+## Development Installation
+
+Install using the following instructions within a virtual environment. 
+```
+git clone https://github.com/troiwill/minigrid-json     # Clone the Minigrid-JSON
+cd minigrid-json                                        # Go into the directory
+pip install -e .                                        # Pip install the package
+```
+
+## Test Scripts
+
+To run the test scripts, run the following code:
+```
+cd tests
+python gen_minigrid_from_img.py
+python gen_minigrid_from_json.py
+```
--- image2minigrid/grid_generator.py
+++ image2minigrid/grid_generator.py
@@ -1,88 +1,103 @@
 from __future__ import annotations
+import copy
+from typing import Tuple, Callable
+
 import cv2
+from minigrid.core.grid import Grid
 import numpy as np
 import skimage.measure
 
-from gymnasium import spaces
-from minigrid.core.belief_grid import BeliefGrid
-from minigrid.core.world_object import Goal, Lava
+from minigrid.core.world_object import Goal, Lava, WorldObj
 from minigrid.manual_control import ManualControl
-from minigrid.minigrid_env import MiniGridEnv
-from minigrid.core.mission import MissionSpace
 
 
-class ImageToGrid(MiniGridEnv):
-
-    def __init__(
-        self,
-        env_img: np.ndarray,
-        agent_start_pos: tuple[int, int] = (2,58), # Make sure the start position doesn't overlap with any obstacles
-        agent_start_dir: int = 0,
-        goal_pos: tuple[int, int] | None = None, 
-        width: int = 80, 
-        height: int = 60,
-        max_steps: int = 1000, 
-        **kwargs
-    ):  
-        self.env_img = env_img
-        self.agent_start_pos = agent_start_pos
-        self.agent_start_dir = agent_start_dir
-        self.goal_pos = goal_pos 
-
-        super().__init__(
-            mission_space = MissionSpace(mission_func=self._gen_mission),
-            width = width, 
-            height = height, 
-            max_steps = max_steps,
-            **kwargs
-        )
+class Image2MinigridGenerator:
 
     @staticmethod
-    def _gen_mission():
-        return "get to the goal safely"
+    def convert_image_to_grid(
+        image: np.ndarray,
+        width: int,
+        height: int,
+        goal_pos: Tuple[int, int] | None = None,
+        free_space_color: Tuple[int, int, int] = (253, 183, 147),
+        occupied_cell_obj: WorldObj = Lava(),
+        grid_fn: Callable[[int, int], Grid] = Grid,
+    ) -> Grid:
+        """
+        Converts an image to a Minigrid Grid object.
 
-    def _gen_grid(self, width: int, height: int) -> None: 
-        self.grid = BeliefGrid(width, height)
+        This method processes an input image and converts it into a Grid object for use in Minigrid environments.
+        It identifies areas of 'land' based on a specified color threshold.
 
-        # Generate the surrounding walls
-        self.grid.wall_rect(0, 0, self.width, self.height)
+        Args:
+            image (np.ndarray): The input image as a NumPy array.
+            width (int): The desired width of the output grid.
+            height (int): The desired height of the output grid.
+            goal_pos (Tuple[int, int] | None, optional): The position of the goal in the grid. Defaults to None.
+            free_space_color (Tuple[int, int, int], optional): The RGB color representing free space in the image.
+                                                               Defaults to (253, 183, 147).
+            occupied_cell_obj (WorldObj, optional): The object to use in occupied cells.
+            grid_fn (Callable[[int, int], Grid], optional): A function to create the grid. Defaults to Grid.
 
-        # Blue Channel Threshold to be consider water 
-        blue_color = np.array([253, 183, 147])
+        Returns:
+            Grid: A Minigrid Grid object representing the processed image.
 
-        img = self.env_img
+        Raises:
+            ValueError: If the image is not a NumPy array, or if height or width are not positive integers.
 
-        # Trim image to fit make it a multiple of x_dim and y_dim 
-        img = img[:(img.shape[0] // self.height) * self.height, :(img.shape[1] // self.width) * self.width]
+        Note:
+            - The method uses min pooling to downscale the image to the desired grid dimensions.
+            - Areas not matching the free_space_color are converted to Lava in the grid.
+            - The grid is surrounded by walls.
+            - If a goal_pos is provided, a Goal object is placed at that position in the grid.
+        """
+
+        if not isinstance(image, np.ndarray):
+            raise ValueError(f"image must be an NDArray. Got type {type(image)}.")
+        height, width = int(height), int(width)
+        if height <= 0:
+            raise ValueError("height must be a positive integer.")
+        if width <= 0:
+            raise ValueError("width must be a positive integer.")
+
+        # Blue Channel Threshold to be consider water
+        threshold_color = np.array(free_space_color)
+
+        # Trim image to fit make it a multiple of x_dim and y_dim
+        img = image.copy()
+        img = img[
+            : (img.shape[0] // height) * height, : (img.shape[1] // width) * width
+        ]
 
         # Discrimate pixels of our desired color
-        img = (img[:, :, 0] == blue_color[0]) & (img[:, :, 1] == blue_color[1]) & (img[:, :, 2] == blue_color[2])
+        img = (
+            (img[:, :, 0] == threshold_color[0])
+            & (img[:, :, 1] == threshold_color[1])
+            & (img[:, :, 2] == threshold_color[2])
+        )
         img = (img).astype(np.uint8)
         img = img * 255
 
         # Min Pooling to downscale image
-        img = skimage.measure.block_reduce(img, (int(img.shape[0] / self.height), int(img.shape[1] / self.width)), np.min)
+        img = skimage.measure.block_reduce(
+            img, (int(img.shape[0] / height), int(img.shape[1] / width)), np.min
+        )
 
         coordinates_of_land = np.argwhere(img == 0)
         list_land = list(map(tuple, coordinates_of_land))
-        for tup in list_land: 
+        grid = grid_fn(width, height)
+        grid.wall_rect(0, 0, width, height)
+        for tup in list_land:
             # Ignore if it already on the border
-            if tup[0] != 0 and tup[0] != height - 1 and tup[1] != 0 and tup[1] != width - 1:
-                self.grid.set(tup[1], tup[0], Lava())
+            if (
+                tup[0] != 0
+                and tup[0] != height - 1
+                and tup[1] != 0
+                and tup[1] != width - 1
+            ):
+                grid.set(tup[1], tup[0], copy.deepcopy(occupied_cell_obj))
 
-        # If an agent start position is not definied, we will look for an empty spot
-        self.agent_pos = self.agent_start_pos 
-        self.agent_dir = self.agent_start_dir
+        if goal_pos is not None:
+            grid.set(goal_pos[0], goal_pos[1], Goal())
 
-        if self.goal_pos is not None:
-            self.put_obj(Goal(), self.goal_pos[0], self.goal_pos[1])
-
-    
-# Example Testing Function 
-# Alter or remove this based on what you're planning on doing    
-def main():
-    env = ImageToGrid(cv2.imread("env_images/st_thomas_no_labels.png"), width = 120, height = 80, render_mode="human")
-    manual_control = ManualControl(env, seed=42)
-    manual_control.start()
-if __name__ == "__main__":
-    main()
+        return grid
